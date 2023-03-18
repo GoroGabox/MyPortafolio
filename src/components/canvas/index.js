e45e8fbebda8cb10755173b0bd8b20cb43b57ce7
GoroGabox <alfons.diaz97@gmail.com>
2023-03-18 16:42:12 -0300

    optimizacion 5

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,5 @@
 import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import LandingCanvas from "./Landing";
-import StarsCanvas from "./Stars";
 
-export { EarthCanvas, BallCanvas, LandingCanvas, StarsCanvas };
+export { EarthCanvas, BallCanvas, LandingCanvas };
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, LandingCanvas, StarsCanvas } from './canvas';
+import { EarthCanvas, BallCanvas, LandingCanvas} from './canvas';
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -18,6 +18,5 @@ export {
   CanvasLoader,
   EarthCanvas, 
   BallCanvas, 
-  LandingCanvas, 
-  StarsCanvas
+  LandingCanvas,
 };
